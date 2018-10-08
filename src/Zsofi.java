fe7ba75a5112a07c18f10fdbd37c802f4d26cbca
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-10-08 16:55:25 +0200

    c5

--- src/Zsofi.java
+++ src/Zsofi.java
@@ -1,17 +1,11 @@
 public class Zsofi {
-<<<<<<< HEAD
-
     public String cheer (){
         return "Alléz la bleu";}
 
         public String cheer (String toPrint){
             toPrint = "Hajrá gyermekeim";
-            return toPrint;
-        }
-=======
-    public String cheer (String toPrint){
-        toPrint = "Hajrá gyermekeim";
-        return toPrint;
+            return toPrint;}
+
     }
->>>>>>> d88b494cd0be4be9614d0b04815059ae4a8818c9
-}
+
+
