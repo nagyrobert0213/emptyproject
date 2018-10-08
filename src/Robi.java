f702fce1b33e1d6dbd903f58e9fb3adbda8c5ce4
cserjesa <andras@cserjes.com>
2018-10-08 16:18:15 +0200

    b1

--- src/Robi.java
+++ src/Robi.java
@@ -1,2 +1,10 @@
 public class Robi {
+
+    public static void main(String[] args) {
+        count();
+    }
+
+    private static void count() {
+        System.out.println("I am 100 years old.");
+    }
 }
