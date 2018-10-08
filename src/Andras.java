052bd3a176ed86009ea9863c0ce26395779d959f
cserjesa <andras@cserjes.com>
2018-10-08 16:44:38 +0200

    bb3

--- src/Andras.java
+++ src/Andras.java
@@ -1,6 +1,7 @@
 public class Andras {
 
-    private static void count() {
+    private static String count() {
         System.out.println("I am 100 years old.");
+        return "This exercise is totally fucked up";
     }
 }
