d88b494cd0be4be9614d0b04815059ae4a8818c9
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-10-08 16:51:21 +0200

    a5

--- src/Robi.java
+++ src/Robi.java
@@ -3,10 +3,7 @@ public class Robi {
     private String name = "Robi";
 
     public void Greet(){
-<<<<<<< HEAD
         System.out.println("Greetings!");
-=======
         System.out.println(name);
->>>>>>> 99df66be2d7d08f9fcfe432fc7685296c585ddc7
     }
 }
