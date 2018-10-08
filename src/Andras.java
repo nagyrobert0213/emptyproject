99df66be2d7d08f9fcfe432fc7685296c585ddc7
nagyrobert0213 <nagyrobert0213@gmail.com>
2018-10-08 16:45:54 +0200

    a4

--- src/Andras.java
+++ src/Andras.java
@@ -1,7 +1,7 @@
 public class Andras {
 
     private static String count() {
-        System.out.println("I am 100 years old.");
+        System.out.println("I am 2 years old.");
         return "This exercise is totally fucked up";
     }
 }
